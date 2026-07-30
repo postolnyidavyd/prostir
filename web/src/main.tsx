4f5612a2502eef93e710377c1276a11a08ede8bf
postolnyidavyd <postolnyi.davyd@student.uzhnu.edu.ua>
2026-07-30 13:04:26 +0300

    chore: ініціалізація web

---
+++ web/src/main.tsx
@@ -0,0 +1,18 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import { App } from './App.tsx';
+import './fonts.css';
+import './index.css';
+
+const root = document.getElementById('root');
+
+if (!root) {
+  throw new Error('Кореневий елемент #root не знайдено');
+}
+
+createRoot(root).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
